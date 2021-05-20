14a0c0a38cef4f8506296cf4c96618c5204d267d
Alexandr Krasnov <sanor00@yandex.ru>
2021-05-20 20:19:26 +0300

    fix & refactoring

--- lab4/Source.cpp
+++ lab4/Source.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-const int numberLen = 4;
+const int MAX_LINT_LEN = 26;
 
 
 /**
@@ -33,7 +33,7 @@ bool DEBUG_logOn = false;
  * @param size 
  * @return string 
  */
-string number2str(int* num, int size) {
+string lInt2str(int* num, int size = MAX_LINT_LEN) {
   stringstream ss;
   bool start_flag = false;
   for (int i = size - 1; i >= 0; i--) {
@@ -51,10 +51,19 @@ string number2str(int* num, int size) {
  * @param size 
  * @return int* zero number
  */
-int* zeroNumber(int size) {
+int* zeroLint(int size = MAX_LINT_LEN) {
   int* res = new int[size];
   for (int i = 0; i < size; i++)
     res[i] = 0;
+  return res;
+}
+
+int* int2lInt(int n, int size = MAX_LINT_LEN) {
+  int* res = zeroLint();
+  for (int i = 0; n != 0 && i < size; i++){
+    res[i] = n % 10;
+    n /= 10;
+  }
   return res;
 }
 
@@ -64,8 +73,8 @@ int* zeroNumber(int size) {
  * @param size 
  * @return int* generated number
  */
-int* genNumber(int size) {
-  int* res = zeroNumber(numberLen);
+int* randLInt(int size = MAX_LINT_LEN) {
+  int* res = zeroLint();
   for (int i = 0; i < size; i++)
     res[i] = rand() % 10;
   return res;
@@ -77,7 +86,7 @@ int* genNumber(int size) {
  * @param a 
  * @param size 
  */
-void fixNumber(int* a, int size) {
+void fixLInt(int* a, int size = MAX_LINT_LEN) {
   for (int i = 0; i < size; i++)
     if (a[i] > 9) {
       a[i + 1] += a[i] / 10;
@@ -94,23 +103,23 @@ void fixNumber(int* a, int size) {
  * @param bSize 
  * @return int* result lInt number
  */
-int* multiply(int* a, int* b, int aSize, int bSize) {
-  int resSize = numberLen;
-  int* res = zeroNumber(resSize);
+int* multiply(int* a, int* b, int aSize = MAX_LINT_LEN, int bSize = MAX_LINT_LEN) {
+  int* res = zeroLint();
   for (int bi = 0; bi < bSize; bi++) {
-    int* tmp = zeroNumber(aSize + 1);
+    int* tmp = zeroLint(aSize + 1);
     for (int ai = 0; ai < aSize; ai++)
       tmp[ai] = a[ai] * b[bi];
-    fixNumber(tmp, aSize + 1);
+    fixLInt(tmp, aSize + 1);
     for (int i = 0; i < aSize + 1; i++)
       res[bi + i] = res[bi + i] + tmp[i];
     delete[] tmp;
   }
-  fixNumber(res, resSize);
+  fixLInt(res);
   return res;
 }
 
 
+int numbers[] = {34, 92, 73, 45, 43, 47, 82, 72, 94, 95, 39, 57, 70, 50};
 /**
  * @brief Master process task
  * @param proc_num the number of slaves initialized
@@ -118,26 +127,35 @@ int* multiply(int* a, int* b, int aSize, int bSize) {
 void masterTask(int proc_num) {
   LOG(INFO, "-Master: started");
   MPI_Datatype lInt;
-  MPI_Type_contiguous(numberLen, MPI_INT, &lInt);
+  MPI_Type_contiguous(MAX_LINT_LEN, MPI_INT, &lInt);
   MPI_Type_commit(&lInt);
 
   for (int i = 0; i < 1; i++) {
     for (int rank_i = 1; rank_i < proc_num; rank_i++) {
-      int* num1 = genNumber(2);
-      int* num2 = genNumber(2);
+      int* num1 = int2lInt(numbers[2 * (rank_i - 1)]);
+      int* num2 = int2lInt(numbers[2*(rank_i - 1) + 1]);
       MPI_Send(num1, 1, lInt, rank_i, 0, MPI_COMM_WORLD);
       MPI_Send(num2, 1, lInt, rank_i, 0, MPI_COMM_WORLD);
 
-      LOG(DEBUG, "-Master: numbers %s and %s sent to proc %d", number2str(num1, numberLen).c_str(), number2str(num2, numberLen).c_str(), rank_i);
+      LOG(DEBUG, "-Master: numbers %s and %s sent to proc %d", lInt2str(num1).c_str(), lInt2str(num2).c_str(), rank_i);
+
+      delete[] num1;
+      delete[] num2;
     }
     LOG(INFO, "-Master: Numbers for all slaves sent (iteration %d)", i);
+    int* ans = int2lInt(1);
     for (int rank_i = 1; rank_i < proc_num; rank_i++) {
-      int* res = new int[numberLen];
+      int* res = new int[MAX_LINT_LEN];
       MPI_Status status;
       MPI_Recv(res, 1, lInt, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-      LOG(DEBUG, "-Master: from proc %d: res: %s", status.MPI_SOURCE, number2str(res, numberLen).c_str());
+      int* tmp = ans;
+      ans = multiply(res, ans);
+      LOG(DEBUG, "-Master: from proc %d: res: %s, curr total: %s", status.MPI_SOURCE, lInt2str(res).c_str(), lInt2str(ans).c_str());
+      delete[] res;
+      delete[] tmp;
     }
-    LOG(INFO, "-Master: Answers from all slaves received (iteration %d)", i);
+    LOG(INFO, "-Master: Answers from all slaves received (iteration %d), total: %s", i, lInt2str(ans).c_str());
+    delete[] ans;
   }
   MPI_Type_free(&lInt);
   return;
@@ -150,29 +168,32 @@ void masterTask(int proc_num) {
 void slaveTask(int rank) {
   LOG(DEBUG, "Slave %d: stared", rank);
   MPI_Datatype lInt;
-  MPI_Type_contiguous(numberLen, MPI_INT, &lInt);
+  MPI_Type_contiguous(MAX_LINT_LEN, MPI_INT, &lInt);
   MPI_Type_commit(&lInt);
   MPI_Status status;
 
+  int* num1 = new int[MAX_LINT_LEN];
+  int* num2 = new int[MAX_LINT_LEN];
   for (int i = 0; i < 1; i++) {
   //while (true) {
-    int* num1 = new int[numberLen];
     MPI_Recv(num1, 1, lInt, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    int* num2 = new int[numberLen];
     MPI_Recv(num2, 1, lInt, 0, 0, MPI_COMM_WORLD, &status);
     LOG(DEBUG, "Slave %d: numbers %s and %s received", rank, 
-      number2str(num1, numberLen).c_str(),
-      number2str(num2, numberLen).c_str());
+      lInt2str(num1).c_str(),
+      lInt2str(num2).c_str());
 
-    int* res = multiply(num1, num2, numberLen, numberLen);
+    int* res = multiply(num1, num2);
     MPI_Send(res, 1, lInt, 0, 0, MPI_COMM_WORLD);
     LOG(INFO, "Slave %d: math done: %s * %s = %s", rank, 
-      number2str(num1, numberLen).c_str(), 
-      number2str(num2, numberLen).c_str(), 
-      number2str(res, numberLen).c_str());
+      lInt2str(num1).c_str(), 
+      lInt2str(num2).c_str(), 
+      lInt2str(res).c_str());
+    delete[] res;
   }
   LOG(DEBUG, "Slave %d: finished", rank);
   MPI_Type_free(&lInt);
+  delete[] num1;
+  delete[] num2;
   return;
 }
 
